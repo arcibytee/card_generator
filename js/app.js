b914287e222a15df540e87919879eec8e4e9fa84
JohnSebastian-24 <juansebastianag24@gmail.com>
2020-12-06 11:48:27 -0500

    added color selection option

--- js/app.js
+++ js/app.js
@@ -10,20 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
         const occupation = document.getElementById('occupation').value;
         const email = document.getElementById('email').value;
         const socialMedia = document.getElementById('socialMedia').value;
+        const cardColor = document.getElementById('cardColor').value;
 
-       // Update the card
-        updateCard(name, occupation, email, socialMedia);
+        // Update the card
+        updateCard(name, occupation, email, socialMedia, cardColor);
 
         // Show the card
         cardContainer.classList.remove('hidden');
     });
 
-    function updateCard(name, occupation, email, socialMedia) {
+    function updateCard(name, occupation, email, socialMedia, cardColor) {
         generatedCard.innerHTML = `
-            <h2>${name}</h2>
-            <p>${occupation}</p>
-            <p>Email: ${email}</p>
-            <p>Social media: ${socialMedia}</p>
+            <h2 style="color: ${cardColor}">${name}</h2>
+            <p style="color: ${cardColor}">${occupation}</p>
+            <p style="color: ${cardColor}">Email: ${email}</p>
+            <p style="color: ${cardColor}">Social media: ${socialMedia}</p>
         `;
     }
 });
