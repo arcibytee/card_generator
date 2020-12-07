a3c56d483037553e496df42943b0c1f17a6e05b3
JohnSebastian-24 <juansebastianag24@gmail.com>
2020-12-07 18:48:27 -0500

    added select image style

--- js/app.js
+++ js/app.js
@@ -13,23 +13,27 @@ document.addEventListener('DOMContentLoaded', function() {
         const cardColor = document.getElementById('cardColor').value;
         const imageInput = document.getElementById('image');
         const image = imageInput.files[0];
+        const imageShape = document.getElementById('imageShape').value;
 
         // Update the card
-        updateCard(name, occupation, email, socialMedia, cardColor, image);
+        updateCard(name, occupation, email, socialMedia, cardColor, image, imageShape);
 
         // Show the card
         cardContainer.classList.remove('hidden');
     });
 
-    function updateCard(name, occupation, email, socialMedia, cardColor, image) {
+    function updateCard(name, occupation, email, socialMedia, cardColor, image, imageShape) {
         const imageURL = image ? URL.createObjectURL(image) : '';
+
+        // Style image
+        const imageStyle = imageShape === 'circle' ? 'border-radius: 50%;' : '';
 
         generatedCard.innerHTML = `
             <h2 style="color: ${cardColor}">${name}</h2>
             <p style="color: ${cardColor}">${occupation}</p>
             <p style="color: ${cardColor}">Email: ${email}</p>
             <p style="color: ${cardColor}">Social media: ${socialMedia}</p>
-            <img src="${imageURL}" alt="User Image" style="max-width: 100%; height: auto;">
+            <img src="${imageURL}" alt="User Image" style="${imageStyle}">
         `;
     }
 });
