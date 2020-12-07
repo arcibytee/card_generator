4d1f451b6d18211a27465ad0d6df57b844c13408
JohnSebastian-24 <juansebastianag24@gmail.com>
2020-12-07 11:48:27 -0500

    added img upload option

--- js/app.js
+++ js/app.js
@@ -11,20 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
         const email = document.getElementById('email').value;
         const socialMedia = document.getElementById('socialMedia').value;
         const cardColor = document.getElementById('cardColor').value;
+        const imageInput = document.getElementById('image');
+        const image = imageInput.files[0];
 
         // Update the card
-        updateCard(name, occupation, email, socialMedia, cardColor);
+        updateCard(name, occupation, email, socialMedia, cardColor, image);
 
         // Show the card
         cardContainer.classList.remove('hidden');
     });
 
-    function updateCard(name, occupation, email, socialMedia, cardColor) {
+    function updateCard(name, occupation, email, socialMedia, cardColor, image) {
+        const imageURL = image ? URL.createObjectURL(image) : '';
+
         generatedCard.innerHTML = `
             <h2 style="color: ${cardColor}">${name}</h2>
             <p style="color: ${cardColor}">${occupation}</p>
             <p style="color: ${cardColor}">Email: ${email}</p>
             <p style="color: ${cardColor}">Social media: ${socialMedia}</p>
+            <img src="${imageURL}" alt="User Image" style="max-width: 100%; height: auto;">
         `;
     }
 });
